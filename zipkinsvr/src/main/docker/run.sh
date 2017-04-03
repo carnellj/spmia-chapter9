5dd691ee8a398e62eb593804acde043451223b3b
carnellj <john.carnell@inin.com>
2017-04-02 20:31:11 -0400

    Cleaned up the launch script

--- zipkinsvr/src/main/docker/run.sh
+++ zipkinsvr/src/main/docker/run.sh
@@ -3,5 +3,7 @@
 echo "********************************************************"
 echo "Starting the Zipkin Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \                         \
-     -Dspring.profiles.active=$PROFILE -jar /usr/local/zipkinserver/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
+     -Dspring.profiles.active=$PROFILE                          \
+      -Dspring.zipkin.baseUrl=$ZIPKIN_URI                       \
+     -jar /usr/local/zipkinserver/@project.build.finalName@.jar
